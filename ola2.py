1a2bba939ea6a6ae4713147632d20ddbaaa150b3
laeknishendr <laeknishendr314@gmail.com>
2015-03-04 18:35:00 +0100

    Update ola2.py

--- ola2.py
+++ ola2.py
@@ -6,3 +6,4 @@ ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
 ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola """
 for d in de: 
   print("dede")
+  #eres un soquete y un tolete
