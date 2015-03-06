18d8db78909763b7bf46036d4b55a6d1842d2d54
augsalmur <asmmochito@hotmail.com>
2015-03-06 09:07:57 +0100

    Delete ola2.py

--- ola2.py
+++
@@ -1,9 +0,0 @@
-"""ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
-ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
-ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
-ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
-ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola 
-ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola ola """
-for d in de: 
-  print("dede")
-  #eres un soquete y un tolete
