fe9b1c128a8e9a1a272cdf45f1b0ea46fcb1b8d7
mariabg92 <mariabg_92@hotmail.com>
2015-03-10 14:28:50 +0100

    carpeta para el trabajo

---
+++ trabajo sos/src/es/us/sos/Trabajo_sosServlet.java
@@ -0,0 +1,13 @@
+package es.us.sos;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Trabajo_sosServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
