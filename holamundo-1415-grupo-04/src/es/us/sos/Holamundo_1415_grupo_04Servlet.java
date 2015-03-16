fc38f5cc214d7de5955a79916b2c509814aba80e
manu <laeknishendr314@gmail.com>
2015-03-16 12:34:50 +0100

    Versión definitiva de la tarea de la práctica 2.

--- holamundo-1415-grupo-04/src/es/us/sos/Holamundo_1415_grupo_04Servlet.java
+++ holamundo-1415-grupo-04/src/es/us/sos/Holamundo_1415_grupo_04Servlet.java
@@ -1,5 +1,7 @@
 package es.us.sos;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class Holamundo_1415_grupo_04Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Buenas noches");
-	}
+		resp.setContentType("text/plain"); 
+		String saludo = req.getPathInfo();
+		if(saludo.toLowerCase().contains("manu")
+				||saludo.toLowerCase().contains("mariajose")
+				||saludo.toLowerCase().contains("augusto")){
+		
+			resp.getWriter().println("Hola "+saludo.substring(1, saludo.length())+"!");
+		
+		}
+		else{
+			resp.getWriter().println("<html><body><h1>Error 404</h1></body></html>");
+		}
+		}
 }
