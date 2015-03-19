8e0c57c834a510e755e333999743f65072ed45f0
mariabg92 <mariabg_92@hotmail.com>
2015-03-19 12:43:31 +0100

    mj

--- prueba/src/es/us/sos/PruebaServlet.java
+++ prueba/src/es/us/sos/PruebaServlet.java
@@ -1,6 +1,7 @@
 package es.us.sos;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -8,6 +9,6 @@ public class PruebaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
-		resp.getWriter().println("<html><body><h1>HOLA MUNDOOO3453454</h1></body></html>");
+		resp.getWriter().println("<html><body><h1>HOLA MUNDO! (por Get)</h1></body></html>");
 	}
 }
