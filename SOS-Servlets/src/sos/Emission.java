2e4118ebb0d4610bbbd761a53356800d3b1c0dd1
manu <laeknishendr314@gmail.com>
2015-03-26 16:39:12 +0100

    emissionsservlet + emission

---
+++ SOS-Servlets/src/sos/Emission.java
@@ -0,0 +1,26 @@
+package sos;
+
+public class Emission {
+	
+	String country; 
+	Double CO2emissions; 
+	Integer population; 
+	Integer year;
+	
+	public Emission(String country, Double cO2emissions, Integer population,
+			Integer year) {
+		super();
+		this.country = country;
+		CO2emissions = cO2emissions;
+		this.population = population;
+		this.year = year;
+	} 
+	
+	public Emission(){
+		
+		
+	}
+	
+	
+
+}
